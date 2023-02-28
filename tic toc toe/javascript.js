d98f9503b9012dc1e67304d22fabf5571fb3f3f5
ROHIT NITTAWADEKAR <rohitnittawadekar07@gmail.com>
2023-02-28 07:18:41 -0800

    some changes for win proper p;;ay result

--- tic toc toe/javascript.js
+++ tic toc toe/javascript.js
@@ -1,5 +1,5 @@
 // document.write("Hello, I'm developer Rohit")
-
+console.log("hello, I'm developer Rohit")
 alert("First Play in X player")
 
 var c = 1;
@@ -16,7 +16,22 @@ function clc(val) {
 
 
         if (check()) {
-            alert("winner!!!");
+            // alert("winner!!!");
+
+
+            var r = document.getElementById(val.id).innerHTML
+            console.log(r)
+
+
+            if (r == 'X'){
+                alert("player 1 is winner!!!");
+                // document.getElementById('result').value("player 1")
+            }else{
+                alert("player 2 is winner!!!");
+                // document.getElementById('result').value("player 2")
+            }
+
+            
             clear();
         }
     } else {
@@ -27,7 +42,7 @@ function clc(val) {
 
 function clear() {
     for (var i = 1; i <= 9; i++) {
-        document.getElementById('div' + i).innerHTML = " ";
+        document.getElementById('div' + i).innerHTML = "";
         c = 1;
     }
 }
@@ -105,4 +120,22 @@ function check() {
         return true;
     }
 
+    function result(x, y, z){
+        
+    }
+
 }
+
+
+    // if (c % 2 == 0) {
+    //     document.getElementById(result).innerHTML = "O are win ";
+    // } else {
+    //     document.getElementById(result).innerHTML = "X are win";
+        
+    // }
+
+    
+
+    
+
+
